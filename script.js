0532dbe842cb6b78f1a7d60444ce3f5e9bd1d12b
Usernamenathanrossisnotavailable <166026893+Usernamenathanrossisnotavailable@users.noreply.github.com>
2024-04-20 08:37:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function Movie(title, rating, year) {
 var ul = document.getElementById('VisualList');
 const myButton = document.getElementById("Add");
 myButton.addEventListener("click", addMov);
-function addMov(){
+function addMov() {
   var t = document.getElementById("EntryTitle");
   var r = document.getElementById("EntryRating");
   var y = document.getElementById("EntryYear");
-  if (t.value != ""&&r.value != ""&&y.value != "") {
-    let newMovie = {title:t.value, rating:r.value, year:y.value};
+  if (t.value != "" && r.value != "" && y.value != "") {
+    let newMovie = new Movie(t.value, r.value, y.value);
     movieArray.push(newMovie);
   } else {
     alert("Warning! Incomplete data provided! Movie will not be added to the list.")
@@ -20,11 +20,11 @@ function addMov(){
 }
 const myButton2 = document.getElementById("Show");
 myButton2.addEventListener("click", showMovs);
-function showMovs(){
+function showMovs() {
   ul.innerHTML = "";
   for (let i = 0; i < movieArray.length; i++) {
     var li = document.createElement('li');
-    li.textContent = "Movie: "+movieArray[i].title+", Rating: "+movieArray[i].rating+", Year: "+movieArray[i].year;
+    li.textContent = "Movie: " + movieArray[i].title + ", Rating: " + movieArray[i].rating + ", Year: " + movieArray[i].year;
     ul.appendChild(li);
   }
 }
